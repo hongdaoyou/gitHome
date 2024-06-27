3447313417b21ef68cd105664ddb649c189a9c43
hongdaoyou <daoyouhong@gmail.com>
2024-06-27 16:54:59 +0800

    add-one

---
+++ shell/auto_commit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ${1}="" ];then
+    msg="add-one";
+else
+    msg=${1}
+fi
+
+# $(git add .)
+git add .
+
+git commit -m ${msg}
+
+# git push
--- shell/find_parent.sh
+++ shell/find_parent.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-pid=206140; 
+# pid=206140; 
+pid=${1}
 
-while [ "$pid" -gt 1 ]; do 
-    ppid=$(ps -o ppid= -p $pid); 
+while [ "$pid" -gt 1 ]; do
+    ppid=$(ps -o ppid= -p $pid);
+    # if [ $ppid = "" ]; then exit() fi;
+
     cmd=$(ps --no-headers -o comm -p $pid); 
     echo  "PID: $pid, PPID: $ppid  CMD: $cmd";  
-    pid=$ppid; 
+    pid=$ppid;
 done
