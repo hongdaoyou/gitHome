c2f543783ed7ff3c358715642787e1324f123e02
hongdaoyou <daoyouhong@gmail.com>
2024-07-03 07:39:39 +0800

    2024-07-03add

--- shell/fail-change_english_sentence.sh
+++ shell/fail-change_english_sentence.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ];then
-    filePath="/home/hdy/data/note/0-wen/0-tmp/tmp3" # 使用默认路径
+    filePath=~/note/0-wen/0-tmp/tmp3 # 使用默认路径
 else
     filePath=${1}
 fi
@@ -16,7 +16,7 @@ function format_english_sentence() {
     # sed -i "s/\n(    )/\n    我的\n   $1/" ${filePath}
     # sed -i "s/\n/\n\n\n/" ${filePath}
 
-    sed -i "s/\n/\n\n\n/" /home/hdy/data/note/0-wen/0-tmp/tmp3
+    sed -i "s/\n/\n\n\n/" ~/data/note/0-wen/0-tmp/tmp3
 }
 
 format_english_sentence
