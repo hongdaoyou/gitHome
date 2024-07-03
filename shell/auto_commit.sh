0db7277ae7fd73f7983c7ae9f7f65cf1350567d5
hongdaoyou <daoyouhong@gmail.com>
2024-07-03 10:37:35 +0800

    2024-07-03-上午-add

--- shell/auto_commit.sh
+++ shell/auto_commit.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 # 自动提交
 
+timeVal=$(date +"%H")
+
+if [ $timeVal -lt 5 ];then
+    timeMsg="凌晨"
+elif [ $timeVal -lt 12 ];then
+    timeMsg="上午"
+elif [ $timeVal -lt 19 ];then
+    timeMsg="下午"
+else
+    timeMsg="晚上"
+fi
+
+# exit 1;
+
+# 注释
 if [ ${1}="" ];then
-    msg=$(date +"%Y-%m-%d")"-add";
+    msg=$(date +"%Y-%m-%d")"-${timeMsg}-add";
 
 else
     msg=${1}
 fi
 
- 
 git add .
 
 git commit -m ${msg}
 
-git push
+# commit成功后,才提交
+if [ $? -eq 0 ];then
+    git push
+fi
+
