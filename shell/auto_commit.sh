8460970595c7e730371bd16620a352ac1c80fe22
hongdaoyou <daoyouhong@gmail.com>
2024-07-06 15:10:06 +0800

    2024-07-06-下午-add

--- shell/auto_commit.sh
+++ shell/auto_commit.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # 自动提交
 
+# 默认,提交到 gitHome. 否则,提交到,指定的目录
+if [ $# -eq 0 ];then
+    cd ~/data/gitHome
+else
+    echo "AAA"
+    cd ${1} || exit 1;
+fi
+
+
+
 timeVal=$(date +"%H")
 
 if [ $timeVal -lt 5 ];then
@@ -21,6 +31,7 @@ if [ ${1}="" ];then
 else
     msg=${1}
 fi
+
 
 git add .
 
