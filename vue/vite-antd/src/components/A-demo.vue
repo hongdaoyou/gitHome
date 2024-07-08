3e8539242fcdfd2b8ed944ac25d08feac1d74318
hongdaoyou <daoyouhong@gmail.com>
2024-07-08 18:15:42 +0800

    2024-07-08-下午-add

---
+++ vue/vite-antd/src/components/A-demo.vue
@@ -0,0 +1,26 @@
+
+<template>
+AA
+{{ a }}
+<button @click="fun">fff</button>
+
+</template>
+
+
+<script setup>
+
+import { ref } from "vue";
+import * as vue from "vue";
+
+let a = ref(0);
+
+function fun(){
+    
+    a.value++;
+//    console.log(1);
+//    alert(2);
+    return 3;
+}
+
+</script>
+
---
+++ vue/vite-antd/src/components/bind_and_class.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <a-button>ddd</a-button>
+AA
+{{ a }}
+<button @click="fun">fff</button>
+
+<div :class="'c1'" class="c2">11</div>
+</template>
+
+
+<script setup>
+
+import { ref } from "vue";
+import * as vue from "vue";
+
+let a = ref(0);
+// let c1="c1";
+
+function fun(){
+    
+    a.value++;
+//    console.log(1);
+//    alert(2);
+    return 3;
+}
+
+</script>
+
+<style scoped>
+.c1 {
+    background: blue;
+    color: red;
+}
+
+.c2 {
+    background: green;
+    /* color: purple; */
+    font-size: 40px;
+}
+
+</style>
