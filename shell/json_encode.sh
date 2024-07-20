5f7dc16b40051a0c487811bdfb77d006862b5895
hongdaoyou <daoyouhong@gmail.com>
2024-07-20 10:34:28 +0800

    2024-07-20-上午-add

---
+++ shell/json_encode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+function json_encode() {
+    
+    # 简化
+    if [ $# -eq 2 ];then
+        indentVal=0;
+    else
+        indentVal=2;
+    fi
+    # echo $indentVal;
+
+    # 如果是,文件. 就直接操作
+    if [ "$1" != "." -o -f "$1" ];then
+        jq --indent ${indentVal} "." $1 
+    else
+        # echo $1 | jq --indent ${indentVal}  "."
+        echo $(cat) | jq --indent ${indentVal}  "."
+    fi
+}
+
+
+json_encode $1 $2
+
+
