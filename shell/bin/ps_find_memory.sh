29cb65b90720ab9e0a8b3940790871b86ec3f664
hongdaoyou <daoyouhong@gmail.com>
2024-08-21 15:07:19 +0800

    2024-08-21-下午-add

--- shell/bin/ps_find_memory.sh
+++ shell/bin/ps_find_memory.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# set -x
+# 
+function fun() {
+    # if [ $# -lt 1 ];then
+    #     echo "请输入,参数"; exit 1;
+    # fi
+    
+    local num=${1:-10}
 
-ps aux | awk 'NR>1 {arr[$11]+=$6} END {for (i in arr) {print arr[i]/1024 " MB", i}}' | sort -nr | head -n 6
+    ps aux | awk 'NR>1 {arr[$11]+=$6} END {for (i in arr) {print arr[i]/1024 " MB", i}}' | sort -nr | head -n $num
+
+}
+
+
+fun "$@"
 
