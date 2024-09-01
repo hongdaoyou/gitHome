ffd67a2366a61008601ae7c61f089c1e4ab3204e
hongdaoyou <daoyouhong@gmail.com>
2024-09-01 23:07:24 +0800

    2024-09-01-晚上-add

--- shell/bin/openssl_request.sh
+++ shell/bin/openssl_request.sh
@@ -6,7 +6,19 @@ function fun() {
     if [ $# -lt 1 ];then
         echo "请输入,参数"; exit 1;
     fi
+    local key=${1:-server.key}
+    if [ -f "$key" ];then
+        echo "私钥文件,不存在"; exit 1;
+    fi
 
+	# openssl req -new -key server.key -out server.csr
+	openssl req -new -key ${key} -out server.csr
+    echo "server.csr"
+
+    # openssl x509 -req -days 36500 -in server.csr -signkey server.key -out server.crt
+    openssl x509 -req -days 36500 -in server.csr -signkey ${key} -out server.crt
+
+    echo "server.crt"
 
 }
 
