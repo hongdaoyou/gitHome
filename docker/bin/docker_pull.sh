62ae2906cf16ecfecc1438cf889db818bbdc5f31
hongdaoyou <daoyouhong@gmail.com>
2024-09-09 09:51:21 +0800

    2024-09-09-上午-add

--- docker/bin/docker_pull.sh
+++ docker/bin/docker_pull.sh
@@ -7,9 +7,14 @@ function fun() {
         echo "请输入,参数"; exit 1;
     fi
     local soft=$1
+    local domain=$2
+
+    if [ -n "$domain" ]; then
+        domain="docker.registry.cyou/library/"
+    fi
     
     # sudo docker pull $soft
-    sudo docker pull docker.registry.cyou/library/$soft
+    sudo docker pull ${domain}${soft}
     
 }
 
