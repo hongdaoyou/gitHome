b277e5646355f1979fe7cfb6dd2f9f79b3edd15a
-g <daoyouhong@gmail.com>
2025-05-29 17:28:34 +0800

    2025-05-29-下午-add

---
+++ shell/UML_yinhao_change.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# set -x
+# 将UML的 双引号,转换成, 单引号
+function fun() {
+    if [ $# -lt 1 ];then
+        echo "请输入,参数"; exit 1;
+    fi
+    local file=$1
+
+
+    sed -i -r "s/\"([':=<\/>1-9a-zA-Z\+\\%\$\^-]+)\"/'\1'/g" $file
+
+    # echo 'aaa' > $file
+# echo 'AA:'$file
+    
+}
+
+
+
+fun "$@"
+
